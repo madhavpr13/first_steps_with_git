2bd8e8d5d81267bd0debdc4e0fd9cbb97713d2d1
Madhav <madhavwas@gmail.com>
2018-12-31 11:11:08 +0530

    Adding my first commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+  public static void main(String[] args){
+
+    System.out.println("Hello, Madhav");
+    System.out.println("Hello, World");
+    System.out.println("1 + 2 is " + (1+2));
+
+  }
+}
